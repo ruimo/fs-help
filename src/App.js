1c499b112971d96dca7260a7b5c6bd6bffde0514
Shisei Hanai <ruimo.uno@gmail.com>
2019-10-13 19:08:56 +0900

    change doc.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import './App.css';
+import HowTo from "./HowTo";
+import {FormattedMessage} from 'react-intl';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <FormattedMessage id="help.text"/>
+        <Route path="/howto" exact render={() => <HowTo/>}/>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {IntlProvider} from "react-intl";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import messages_ja from "./translations/ja.json";
+import messages_en from "./translations/en.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const messages = {
+  'ja': messages_ja,
+  'en': messages_en
+};
+
+const language = navigator.language.split(/[-_]/)[0];  // language without region code
+
+ReactDOM.render(
+  <IntlProvider locale={language} messages={messages[language]}><App/></IntlProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
